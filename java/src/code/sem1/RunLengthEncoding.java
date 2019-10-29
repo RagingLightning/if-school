e97eb097d4f238329285776c4bfd6ff43244fcb6
RagingLightning <raginglightningcode@gmail.com>
2019-10-29 13:09:24 +0100

    191029

--- java/src/code/sem1/RunLengthEncoding.java
+++ java/src/code/sem1/RunLengthEncoding.java
@@ -1,4 +1,12 @@
 package code.sem1;
 
+import de.r13g.lib.Tools;
+import de.r13g.lib.console.ConsoleExt;
+
 public class RunLengthEncoding {
+
+  public static void main(String[] args) {
+    String s = ConsoleExt.getInputString("String");
+    System.out.println(Tools.runLengthEncoding(s, 1));
+  }
 }
--- java/src/de/r13g/lib/AdvancedString.java
+++ java/src/de/r13g/lib/AdvancedString.java
@@ -1,11 +1,47 @@
 package de.r13g.lib;
 
-public class StringTools {
+public class AdvancedString {
 
-  public static String replaceCharAt(String string, int position, char replacement) {
-    char[] array = string.toCharArray();
+  private String value;
+
+  public AdvancedString(Object value) {
+    this.value = String.valueOf(value);
+  }
+
+  public static AdvancedString fromString(String string) {
+    return new AdvancedString(string);
+  }
+
+  @Override
+  public String toString() {return value;}
+
+  public AdvancedString replaceCharAt(int position, char replacement) {
+    char[] array = this.value.toCharArray();
     array[position] = replacement;
-    return new String(array);
+    this.value = new String(array);
+    return this;
+  }
+
+  public AdvancedString repeat(int times) {
+    String original = this.value;
+    this.append(original, times);
+    return this;
+  }
+
+  public AdvancedString append(String string, int times) {
+    for (int i = 0; i < times; i++) {
+      this.value += string;
+    }
+    return this;
+  }
+
+  public int longestRun(int startIdx) {
+    char c = this.value.charAt(startIdx);
+    int i;
+    for (i = 0; i < this.value.length() - startIdx; i++) {
+      if (this.value.charAt(i+startIdx) != c) return i;
+    }
+    return i;
   }
 
 }
