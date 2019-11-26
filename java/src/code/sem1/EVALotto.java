569d086f135c9a551720f724fe5032d3f3cdd4ca
RagingLightning <raginglightningcode@gmail.com>
2019-11-26 13:06:14 +0100

    191126_2

--- java/src/code/sem1/EVALotto.java
+++ java/src/code/sem1/EVALotto.java
@@ -29,22 +29,42 @@ public class EVALotto {
     for (int i = 0; i < random.length; i++) {
       int num = Tools.randomInt(1,49);
       while (Arrays.asList(random).contains(num)) {
-        int num = Tools.randomInt(1,49);
+        num = Tools.randomInt(1,49);
       }
       random[i] = num;
     }
   }
 
   private void printArrays() {
+    System.out.println("-- Zufallszahlen --");
+    System.out.println("-------------------");
+    for (int i = 0; i < random.length; i++) {
+      System.out.println("" + (i+1) + ": " + random[i]);
+    }
+    System.out.println();
 
+    System.out.println("-- Eingabe --");
+    System.out.println("-------------");
+    for (int i = 0; i < user.length; i++) {
+      System.out.println("" + (i+1) + ": " + user[i]);
+    }
   }
 
   private int compareNumbers() {
-
+    int cnt = 0;
+    for (int n : user) {
+      cnt += Arrays.asList(random).contains(n) ? 1 : 0;
+    }
+    return cnt;
   }
 
   private int[] compareRuns(int nRuns) {
-
+    int[] results = new int[7];
+    for (int i = 0; i < nRuns; i++) {
+      randomGeneration();
+      results[compareNumbers()] += 1;
+    }
+    return results;
   }
 
 }
