1ce1010100e3a10bb7d12eab64a9c30b0ffceb72
RagingLightning <RagingLightningCode@gmail.com>
2020-09-03 09:21:44 +0200

    fixed missing files

--- java/src/main/code/sem1/Recursion.java
+++ java/src/main/code/sem1/Recursion.java
@@ -1,4 +1,21 @@
 package code.sem1;
 
 public class Recursion {
+
+  public static void main(String[] args) {
+    new Recursion();
+  }
+
+  public Recursion() {
+    System.out.println(stepAnalysis(50));
+  }
+
+  public long stepAnalysis(int remains) {
+    long n = 0;
+    if (remains == 0) return 1;
+    if (remains >= 2) n += stepAnalysis(remains-2);
+    if (remains >= 1) n += stepAnalysis(remains-1);
+    return n;
+  }
+
 }
